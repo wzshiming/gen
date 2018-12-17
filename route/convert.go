d9dc3241aa0ece72af00f1745581853d8c3a43c2
wzshiming <wzshiming@foxmail.com>
2018-12-17 13:44:34 +0800

    fixes convert

--- route/convert.go
+++ route/convert.go
@@ -5,58 +5,77 @@ import (
 )
 
 func (g *GenRoute) convertString(in, out string, typ *spec.Type) error {
-	g.buf.WriteFormat(`%s = %s`, out, in)
+	g.buf.WriteFormat(`%s = `, out)
+	g.Types(typ)
+	g.buf.WriteFormat(`(%s)`, in)
 	return nil
 }
 
 func (g *GenRoute) convertPrtString(in, out string, typ *spec.Type) error {
-	g.buf.WriteFormat(`%s = &%s`, out, in)
+	g.buf.WriteFormat(`_i := `)
+	g.Types(typ)
+	g.buf.WriteFormat(`(%s)
+	%s = &_i
+`, in, out)
 	return nil
 }
 
 func (g *GenRoute) convertInt64(in, out string, typ *spec.Type) error {
 	g.buf.AddImport("", "strconv")
-	name := typ.Name
 	g.buf.WriteFormat(`if i, err := strconv.ParseInt(%s,0,0); err == nil {
-	%s = %s(i)
+	%s = `, in, out)
+	g.Types(typ)
+	g.buf.WriteString(`(i)
 }
-`, in, out, name)
+`)
 	return nil
 }
 
 func (g *GenRoute) convertPrtInt64(in, out string, typ *spec.Type) error {
 	g.buf.AddImport("", "strconv")
-	name := typ.Name
 	g.buf.WriteFormat(`if i, err := strconv.ParseInt(%s,0,0); err == nil {
-	_i := %s(i)
+	_i := `, in)
+	g.Types(typ)
+	g.buf.WriteFormat(`(i)
 	%s = &_i
 }
-`, in, name, out)
+`, out)
 	return nil
 }
 
 func (g *GenRoute) convertUint64(in, out string, typ *spec.Type) error {
 	g.buf.AddImport("", "strconv")
-	name := typ.Name
 	g.buf.WriteFormat(`if i, err := strconv.ParseUint(%s,0,0); err == nil {
-	%s = %s(i)
+	%s = `, in, out)
+	g.Types(typ)
+	g.buf.WriteString(`(i)
 }
-`, in, out, name)
+`)
 	return nil
 }
 
 func (g *GenRoute) convertPrtUint64(in, out string, typ *spec.Type) error {
 	g.buf.AddImport("", "strconv")
-	name := typ.Name
 	g.buf.WriteFormat(`if i, err := strconv.ParseUint(%s,0,0); err == nil {
-	_i := %s(i)
+	_i := `, in)
+	g.Types(typ)
+	g.buf.WriteFormat(`(i)
 	%s = &_i
 }
-`, in, name, out)
+`, out)
+	return nil
+}
+
+func (g *GenRoute) convertBytes(in, out string, typ *spec.Type) error {
+	g.buf.WriteFormat(`%s := `, out)
+	g.Types(typ)
+	g.buf.WriteFormat(`(%s)
+`, in)
 	return nil
 }
 
 func (g *GenRoute) Convert(in, out string, typ *spec.Type) error {
+	ptyp := typ
 	if typ.Ref != "" {
 		typ = g.api.Types[typ.Ref]
 	}
@@ -64,30 +83,31 @@ func (g *GenRoute) Convert(in, out string, typ *spec.Type) error {
 	switch typ.Kind {
 	case spec.Ptr:
 		typ = typ.Elem
+		ptyp = typ
 		if typ.Ref != "" {
 			typ = g.api.Types[typ.Ref]
 		}
 		switch typ.Kind {
 		case spec.String:
-			return g.convertPrtString(in, out, typ)
+			return g.convertPrtString(in, out, ptyp)
 		case spec.Int8, spec.Int16, spec.Int32, spec.Int64, spec.Int:
-			return g.convertPrtInt64(in, out, typ)
+			return g.convertPrtInt64(in, out, ptyp)
 		case spec.Uint8, spec.Uint16, spec.Uint32, spec.Uint64, spec.Uint:
-			return g.convertPrtUint64(in, out, typ)
+			return g.convertPrtUint64(in, out, ptyp)
 		default:
 		}
 	default:
 		switch typ.Kind {
 		case spec.String:
-			return g.convertString(in, out, typ)
+			return g.convertString(in, out, ptyp)
 		case spec.Int8, spec.Int16, spec.Int32, spec.Int64, spec.Int:
-			return g.convertInt64(in, out, typ)
+			return g.convertInt64(in, out, ptyp)
 		case spec.Uint8, spec.Uint16, spec.Uint32, spec.Uint64, spec.Uint:
-			return g.convertUint64(in, out, typ)
+			return g.convertUint64(in, out, ptyp)
 		case spec.Slice:
-			if typ.Elem.Kind == spec.Byte {
-				g.buf.WriteFormat("%s := []byte(%s)\n", out, in)
-				return nil
+			switch typ.Elem.Kind {
+			case spec.Byte, spec.Rune:
+				return g.convertBytes(in, out, ptyp)
 			}
 		}
 	}
