4dcd880a6a137728c5c372f3549514f553f1f568
wzshiming <wzshiming@foxmail.com>
2018-12-19 14:01:17 +0800

    fixes get tag

--- parser/util.go
+++ parser/util.go
@@ -20,16 +20,16 @@ func GetName(t string, tag reflect.StructTag) string {
 // GetTag [#[^#]+#]...
 func GetTag(text string) reflect.StructTag {
 	ss := []string{}
-	prev := 0
+	prev := -1
 	for i, v := range text {
 		if v != '#' {
 			continue
 		}
-		if prev == 0 {
+		if prev == -1 {
 			prev = i
 		} else {
 			ss = append(ss, text[prev+1:i])
-			prev = 0
+			prev = -1
 		}
 	}
 	return reflect.StructTag(strings.Join(ss, " "))
