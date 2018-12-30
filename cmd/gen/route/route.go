8cfed7658186ec1724ddacfc134bcd597eb4402e
wzshiming <wzshiming@foxmail.com>
2018-12-30 19:23:52 +0800

    route add openapi

--- cmd/gen/route/route.go
+++ cmd/gen/route/route.go
@@ -1,20 +1,30 @@
 package route
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"path/filepath"
 
 	"github.com/spf13/cobra"
+	"github.com/wzshiming/gen/openapi"
 	"github.com/wzshiming/gen/parser"
 	"github.com/wzshiming/gen/route"
 	"github.com/wzshiming/gen/utils"
 	"github.com/wzshiming/gotype"
+	oaspec "github.com/wzshiming/openapi/spec"
+	"github.com/wzshiming/openapi/util"
 )
 
 var (
-	out  string
-	name string
-	pack string
+	out      string
+	name     string
+	pack     string
+	servers  []string
+	format   string
+	info     string
+	openapiF bool
 )
 
 func init() {
@@ -22,7 +32,10 @@ func init() {
 	flag.StringVarP(&out, "out", "o", "router_gen.go", "output file name")
 	flag.StringVarP(&name, "name", "n", "Router", "routing function name")
 	flag.StringVarP(&pack, "package", "p", "", "package name")
-
+	flag.BoolVarP(&openapiF, "openapi", "", false, "with openapi")
+	flag.StringSliceVarP(&servers, "servers", "s", nil, "")
+	flag.StringVarP(&format, "format", "f", "json", "json or yaml")
+	flag.StringVarP(&info, "info", "i", "", "Info")
 }
 
 var Cmd = &cobra.Command{
@@ -50,6 +63,51 @@ var Cmd = &cobra.Command{
 		if err != nil {
 			return err
 		}
+
+		if openapiF {
+
+			var oainfo *oaspec.Info
+
+			if info != "" {
+				fil, err := ioutil.ReadFile(info)
+				if err != nil {
+					return err
+				}
+				fil, err = util.YAML2JSON(fil)
+
+				if err != nil {
+					return err
+				}
+				err = json.Unmarshal(fil, &oainfo)
+				if err != nil {
+					return err
+				}
+			}
+
+			api, err := openapi.NewGenOpenAPI(def.API()).WithServices(servers...).SetInfo(oainfo).Generate()
+			if err != nil {
+				return err
+			}
+
+			dc, err := json.MarshalIndent(api, "", " ")
+			if err != nil {
+				return err
+			}
+			switch format {
+			case "json":
+
+			case "yaml":
+				dc, err = util.JSON2YAML(dc)
+				if err != nil {
+					return err
+				}
+			default:
+				return fmt.Errorf("undefined format %s", format)
+			}
+
+			d.WriteFormat("var OpenAPI=`%s`", string(dc))
+		}
+
 		return d.WithFilename(out).Save()
 	},
 }
