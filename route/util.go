bcfadf45b736ccb901cc2df2f23e120f23329bbf
wzshiming <wzshiming@foxmail.com>
2019-01-10 22:31:18 +0800

    change name

--- route/util.go
+++ route/util.go
@@ -11,37 +11,36 @@ import (
 func (g *GenRoute) GetVarName(name string) string {
 	if name == "err" {
 		return name
-
 	}
 	return "_" + namecase.ToLowerHumpInitialisms(fmt.Sprintf("var_%s", name))
 }
 
 func (g *GenRoute) GetRouteName(typ *spec.Type) string {
-	name := namecase.ToPascal(fmt.Sprintf("route_%s", typ.Name))
+	name := namecase.ToUpperHumpInitialisms(fmt.Sprintf("route_%s", typ.Name))
 	addr := unsafe.Pointer(typ)
 	return g.named.GetName(name, addr)
 }
 
 func (g *GenRoute) GetOperationFunctionName(oper *spec.Operation) string {
-	name := "_" + namecase.ToCamel(fmt.Sprintf("operation_%s_%s", oper.Method, oper.Path))
+	name := "_" + namecase.ToLowerHumpInitialisms(fmt.Sprintf("operation_%s_%s", oper.Method, oper.Path))
 	addr := unsafe.Pointer(oper)
 	return g.named.GetName(name, addr)
 }
 
 func (g *GenRoute) GetRequestFunctionName(req *spec.Request) string {
-	name := "_" + namecase.ToCamel(fmt.Sprintf("request_%s_%s", req.In, req.Name))
+	name := "_" + namecase.ToLowerHumpInitialisms(fmt.Sprintf("request_%s_%s", req.In, req.Name))
 	addr := unsafe.Pointer(req)
 	return g.named.GetName(name, addr)
 }
 
 func (g *GenRoute) GetSecurityFunctionName(secu *spec.Security) string {
-	name := "_" + namecase.ToCamel(fmt.Sprintf("security_%s_%s", secu.Schema, secu.Name))
+	name := "_" + namecase.ToLowerHumpInitialisms(fmt.Sprintf("security_%s_%s", secu.Schema, secu.Name))
 	addr := unsafe.Pointer(secu)
 	return g.named.GetName(name, addr)
 }
 
 func (g *GenRoute) GetMiddlewareFunctionName(midd *spec.Middleware) string {
-	name := "_" + namecase.ToCamel(fmt.Sprintf("middleware_%s_%s", midd.Schema, midd.Name))
+	name := "_" + namecase.ToLowerHumpInitialisms(fmt.Sprintf("middleware_%s_%s", midd.Schema, midd.Name))
 	addr := unsafe.Pointer(midd)
 	return g.named.GetName(name, addr)
 }
